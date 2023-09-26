ab70a6ac0445f96235b97b865d59e62d5b559781
ECJUNG <gatas88@naver.com>
2023-09-26 23:17:09 +0900

    randomGame7

--- main.js
+++ main.js
@@ -12,9 +12,16 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chanceArea = document.getElementById("chance-area");
 let count = 0;
+let chance = 5;
+let gameOver = false;
+let history=[];
 
 playButton.addEventListener("click",play)
+resetButton.addEventListener("click",reset)
+userInput.addEventListener("focus",function(){userInput.value = "";})
 
 function pickRandomNum(){
     computerNum = Math.floor(Math.random() * 100)+1;
@@ -23,6 +30,25 @@ function pickRandomNum(){
 
 function play(){
     let userValue = userInput.value;
+
+    // if(userValue > 100){
+    //     resultArea.textContent = "100 이하의 숫자를 입력 해 주세요"
+    // } else if(userValue <= 0){
+    //     resultArea.textContent = "1 이상의 숫자를 입력 해 주세요"
+    // }
+
+    if(userValue<1 || userValue>100){
+        resultArea.textContent = "1과 100 사이의 숫자를 입력해 주세요"
+        return;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요."
+        return;
+    }
+
+    chance--;
+    chanceArea.textContent=`남은 기회 : ${chance}번`
     
     if(userValue < computerNum) {
        resultArea.textContent = "Up!!!"
@@ -32,9 +58,33 @@ function play(){
         count++;
     } else {
         count++
-        resultArea.textContent ="정답 입니다." + count + "번만에 맞추셨습니다."
-
+        resultArea.textContent =`정답 입니다.${count}번만에 맞추셨습니다.`
+        playButton.disabled = true;
     }
+
+    history.push(userValue);
+    console.log(history);
+
+    if (chance < 1) {
+        gameOver = true;
+    }
+
+    if (gameOver == true) {
+        playButton.disabled = true;
+    }
+}
+
+function reset(){
+    //user input창이 꺠끗하게 정리
+    userInput.value = "";
+    count = 0;
+    chance = 5;
+    chanceArea.textContent=`남은 기회 : ${chance}번`
+    resultArea.textContent=`결과가 나온다`;
+    playButton.disabled = false;
+    history.length = 0; // 배열 비우기
+    //새로운 번호가 생성
+    pickRandomNum()
 }
 
 pickRandomNum()
